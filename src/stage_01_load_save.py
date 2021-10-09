cfe4be6f94bbf551a1af95aa7400eef185e07c6b
c17hawke <2016peb5126@mnit.ac.in>
2021-10-09 09:58:21 +0530

    logging added and stage one done

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -39,9 +39,9 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
     try:
-        logging.info("stage one started")
+        logging.info(">>>>> stage one started")
         get_data(config_path=parsed_args.config)
-        logging.info("stage one completed! all the data are saved in local")
+        logging.info("stage one completed! all the data are saved in local >>>>>")
     except Exception as e:
         logging.exception(e)
         raise e
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -6,7 +6,7 @@ import logging
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
-    logging.info("yaml file: {path_to_yaml} loaded successfully")
+    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
     return content
 
 
