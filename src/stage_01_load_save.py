f1fdcd11c21065830e548ccb0bc410dc9bdfb4f4
c17hawke <2016peb5126@mnit.ac.in>
2021-10-09 09:40:31 +0530

    logging created

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -4,7 +4,12 @@ import pandas as pd
 import os
 import shutil
 from tqdm import tqdm
+import logging
 
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+create_directory([log_dir])
+logging.basicConfig(filename=os.path.join(log_dir, "running_logs.log"), level=logging.INFO, format=logging_str, filemode='a')
 
 def copy_file(source_download_dir, local_data_dir):
     list_of_files = os.listdir(source_download_dir)
@@ -32,4 +37,10 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-    get_data(config_path=parsed_args.config)
+    try:
+        logging.info("stage one started")
+        get_data(config_path=parsed_args.config)
+        logging.info("stage one completed! all the data are saved in local")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,26 +1,27 @@
 import yaml
 import os
 import json
-
+import logging
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
+    logging.info("yaml file: {path_to_yaml} loaded successfully")
     return content
 
 
 def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
-        print(f"directory is created at {dir_path}")
+        logging.info(f"directory is created at {dir_path}")
 
 
 def save_local_df(data, data_path, index_status=False):
     data.to_csv(data_path, index=index_status)
-    print(f"data is saved at {data_path}")
+    logging.info(f"data is saved at {data_path}")
 
 
 def save_reports(report: dict, report_path: str, indentation=4):
     with open(report_path, "w") as f:
         json.dump(report, f, indent=indentation)
-    print(f"reports are saved at {report_path}")
+    logging.info(f"reports are saved at {report_path}")
