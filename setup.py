17a6755ef2662ff59b55e41aecea0d123bae0497
c17hawke <2016peb5126@mnit.ac.in>
2021-10-09 09:02:21 +0530

    basic setup done

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="c17hawke",
+    description="A small package for dvc dl pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/c17hawke/DVC_DL_Tensorflow_demo",
+    author_email="sunny.c17hawke@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        "dvc",
+        "tensorflow",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "tqdm",
+        "PyYAML",
+        "boto3" ,
+    ]
+)
