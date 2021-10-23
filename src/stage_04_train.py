565e3297278e0f23fea260d57731b578a8d73145
c17hawke <2016peb5126@mnit.ac.in>
2021-10-23 09:25:36 +0530

    stage 04 base file created

---
+++ src/stage_04_train.py
@@ -0,0 +1,34 @@
+from src.utils.all_utils import read_yaml, create_directory
+import argparse
+import os
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
+
+def train_model(config_path, params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info(">>>>> stage four started")
+        train_model(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info("stage four completed! training completed and model is saved>>>>>")
+    except Exception as e:
+        logging.exception(e)
+        raise e
