1fe50ca8c77d73c3480e4932829c3b3ba41736ca
c17hawke <2016peb5126@mnit.ac.in>
2021-10-23 09:05:59 +0530

    added ckpt callback

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -18,4 +18,12 @@ def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
     
 
 def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
-    pass
+    checkpoint_file_path = os.path.join(checkpoint_dir, "ckpt_model.h5")
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_file_path,
+        save_best_only=True
+    )
+
+    ckpt_callback_filepath = os.path.join(callbacks_dir, "checkpoint_cb.cb")
+    joblib.dump(checkpoint_callback, ckpt_callback_filepath)
+    logging.info(f"tensorboard callback is being saved at {ckpt_callback_filepath}")
